43b35ce3f6c6ca33a47fb6cba1c91ad2df94309b
Alexandre Casaroes <alecasaroes.dev@outlook.com>
2025-05-27 15:47:38 +0100

    add highlight to the active tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,49 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+        screenOptions={{
+			tabBarActiveTintColor: "black",
+        }}
+    >
         <Tabs.Screen 
           name="index" 
           options={{
-          headerTitle: "Weather Day",
+			tabBarIcon: ({focused, color}) => (
+			<Ionicons 
+				name={focused ? "home-sharp" : "home-outline"}
+				color={color}
+				size={20}
+			/>
+			),
           }}
         />
-        <Tabs.Screen name="search"/>
-        <Tabs.Screen name="settings"/>
+        <Tabs.Screen 
+          name="search" 
+          options={{
+			tabBarIcon: ({focused, color}) => (
+			<Ionicons 
+				name={focused ? "search-sharp" : "search-outline"}
+				color={color}
+				size={20}
+			/>
+			),
+          }}
+        />
+        <Tabs.Screen 
+          name="settings" 
+          options={{
+			tabBarIcon: ({focused, color}) => (
+			<Ionicons 
+				name={focused ? "settings-sharp" : "settings-outline"}
+				color={color}
+				size={20}
+			/>
+			),
+          }}
+        />
     </Tabs>
   );
 }
